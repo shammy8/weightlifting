591de4dacf097d5c84ce80c834b85a066553c25f
Allan Sham <allansham@hotmail.com>
2023-06-27 16:07:15 +0100

    install luxon adaptor and try mat calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatLuxonDateModule,
+  ],
   template: `
+    <mat-toolbar color="primary">
+      <span>WL</span>
+    </mat-toolbar>
+
+    <!-- <input matInput [matDatepicker]="picker" />
+    <mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle> -->
+    <!-- <mat-datepicker #picker="matCalendar"></mat-datepicker> -->
+    <div style="width: 500px">
+      <mat-calendar />
+    </div>
+
     <button (click)="test.set('hello')">Change</button>
     {{ test() }} {{ test2() }}
     <router-outlet></router-outlet>
@@ -18,5 +39,7 @@ export class AppComponent {
 
   test = signal('hi');
   test2 = computed(() => this.test() + 'there');
-  test3 = effect(() => {console.log('effect',this.test())})
+  test3 = effect(() => {
+    console.log('effect', this.test());
+  });
 }
